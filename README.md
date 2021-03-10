59385772eb61ad2aa3ccd16a4fe37893eb3ddd87
albertfay1121 <albertfay1121@gmail.com>
2021-03-10 14:11:52 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Aggy
+ Final Project for Udacity C++ Course. Project Aggy will be a game developed with SDL2.
---
+++ build/CMakeFiles/project_aggy.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/project_aggy.dir/src/main.cpp.obj"
+  "libproject_aggy.dll.a"
+  "project_aggy.exe"
+  "project_aggy.exe.manifest"
+  "project_aggy.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/project_aggy.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
