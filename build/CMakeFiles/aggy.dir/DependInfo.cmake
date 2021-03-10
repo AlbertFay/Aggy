83ca2a3443ba8eeeecbe68eb66c8ad5ca679c006
albertfay1121 <albertfay1121@gmail.com>
2021-03-10 14:16:47 -0800

    Updated executable name

---
+++ build/CMakeFiles/aggy.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/C++ Development/C++ Projects/Aggy/src/main.cpp" "C:/C++ Development/C++ Projects/Aggy/build/CMakeFiles/aggy.dir/src/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "SDL_MAIN_HANDLED"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "C:/C++ Development/Libs/SDL2-2.0.14/x86_64-w64-mingw32/include"
+  "../src"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/aggy.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/aggy.dir/src/main.cpp.obj"
+  "aggy.exe"
+  "aggy.exe.manifest"
+  "aggy.pdb"
+  "libaggy.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/aggy.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
