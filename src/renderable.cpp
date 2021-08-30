fe5111628faa0fbebd6984cf96a288777fa761a9
Albert <65504787+albertfay1121@users.noreply.github.com>
2021-08-30 14:51:09 -0700

    Update renderable.cpp

--- src/renderable.cpp
+++ src/renderable.cpp
@@ -3,12 +3,13 @@
 
 bool Renderable::Exists(){
     std::cout << "This needs to be implemented" << std::endl;
+    return true;
 };
 
 void Renderable::RenderRenderable(SDL_Renderer* renderer, ResourceManager &resources){
     std::cout << "This needs to be implemented" << std::endl;
-}
+};
 
 void Renderable::Update() {
     std::cout << "this needs to be implemented" << std:: endl;
-}
+};
